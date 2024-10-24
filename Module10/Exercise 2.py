9fe720a5b4db2376b2fdb0f9ff4ff23a4debdc95
Lion504 <Lehtonen6677@gmail.com>
2024-10-25 00:47:42 +0300

    fix different elevator chosen logic + remove Elevator in Building

--- Module10/Exercise 2.py
+++ Module10/Exercise 2.py
@@ -1,11 +1,11 @@
 class Elevator:
-    def __init__ (self, floor_bottom, floor_top):
+    def __init__(self, floor_bottom, floor_top):
         self.floor_bottom = floor_bottom
         self.floor_top = floor_top
         self.floor_want_go = 0
         self.current_floor = 1
 
-    def elevator_move(self,floor_get):
+    def elevator_move(self, floor_get):
         if floor_get > self.floor_top:
             print(f"Sorry {floor_get} floor is too high to reach!")
         elif floor_get < self.floor_bottom:
@@ -21,13 +21,13 @@ class Elevator:
                 print(f"You are reached {self.floor_want_go} floor")
 
     def move_up(self):
-        for up in range(self.current_floor,self.floor_want_go+1,1):
+        for up in range(self.current_floor, self.floor_want_go + 1, 1):
             print(f"Going up, floor {up} ")
-        self.current_floor= self.floor_want_go
+        self.current_floor = self.floor_want_go
 
     def move_down(self):
-        for down in range(self.current_floor,self.floor_want_go-1,-1):
-            if down == 0: #skip 0
+        for down in range(self.current_floor, self.floor_want_go - 1, -1):
+            if down == 0:  #skip 0
                 continue
             print(f"Going down, floor {down}")
 
@@ -37,31 +37,45 @@ class Elevator:
         self.current_floor = 1
         print("initialise elevator to 1 floor")
 
-class Building(Elevator):
-    def __init__ (self, floor_bottom, floor_top, elevator_num):
+
+class Building():
+    def __init__(self, floor_bottom, floor_top, elevator_num):
         self.floor_bottom = floor_bottom
         self.floor_top = floor_top
         self.elevator_num = elevator_num
-        self.elevator = Elevator(floor_bottom, floor_top)
+        self.elevator = [Elevator(floor_bottom, floor_top) for _ in  range(elevator_num)]# create several class for each elevator
+        self.elevator_used = None
     def run_elevator(self, accept_floor, accept_elevator):
         if accept_elevator > self.elevator_num:
             print(f"No.{accept_elevator} elevator not exist!")
             return
-        elif accept_elevator < 0:
+        elif accept_elevator <= 0:
             print(f"No.{accept_elevator} elevator not exist!")
             return
+
+        elevator = self.elevator[accept_elevator - 1]
+        if self.elevator_used is not None and self.elevator_used == accept_elevator:
+            print(f"start from {self.elevator_used} elevator move from {elevator.current_floor}")
         else:
-            print(f"You chosen No.{accept_elevator} elevator.")
-        self.elevator.elevator_move(accept_floor)
-        
+            print(f"Using elevator {accept_elevator}")
+
+        self.elevator_used = accept_elevator
+        elevator.elevator_move(accept_floor)
 
 
-new_building = Building(-3,8,5)
+new_building = Building(-3, 8, 5)
 
-new_building.run_elevator(2,3)
-new_building.elevator.initial_elevator()
+new_building.run_elevator(2, 3)
+#new_building.elevator.initial_elevator()
+
 #test
-new_building.run_elevator(9,1) #top floor out of range
-new_building.run_elevator(-4,4) #bottom floor out of range
-new_building.run_elevator(-1,2) #bottom floor out of range negative number test
-new_building.run_elevator(2,-1) #elevator out of range negative number test
+new_building.run_elevator(5, 1)
+new_building.run_elevator(2, 1)
+new_building.run_elevator(4, 3)
+new_building.run_elevator(2, 1)
+'''new_building.run_elevator(3, 1)  #top floor out of range
+new_building.run_elevator(-4, 4)  #bottom floor out of range
+new_building.run_elevator(-1, 2)  #bottom floor out of range negative number test
+new_building.run_elevator(5, 5)  #bottom floor out of range negative number test
+
+new_building.run_elevator(2, -1)  #elevator out of range negative number test'''
