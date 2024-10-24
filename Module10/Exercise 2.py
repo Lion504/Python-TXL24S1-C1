97ac45b7abe69da92cc5aafc3cdb1909a703f364
Lion504 <Lehtonen6677@gmail.com>
2024-10-24 23:13:46 +0300

    improve method name

--- Module10/Exercise 2.py
+++ Module10/Exercise 2.py
@@ -5,7 +5,7 @@ class Elevator:
         self.floor_want_go = 0
         self.current_floor = 1
 
-    def go_to_floor(self,floor_get):
+    def elevator_move(self,floor_get):
         if floor_get > self.floor_top:
             print("It's too high to reach!")
         elif floor_get < self.floor_bottom:
@@ -13,20 +13,21 @@ class Elevator:
         else:
             if floor_get > self.current_floor:
                 self.floor_want_go = floor_get
-                self.floor_up()
+                self.move_up()
                 print(f"You are reached {self.floor_want_go} floor")
             elif floor_get < self.current_floor:
                 self.floor_want_go = floor_get
-                self.floor_down()
+                self.move_down()
                 print(f"You are reached {self.floor_want_go} floor")
 
-    def floor_up(self):
+    def move_up(self):
         for up in range(self.current_floor,self.floor_want_go+1,1):
             print(f"Going up, floor {up} ")
         self.current_floor= self.floor_want_go
 
-    def floor_down(self):
+    def move_down(self):
         for down in range(self.current_floor,self.floor_want_go-1,-1):
+
             print(f"Going down, floor {down}")
         self.current_floor = self.current_floor
 
@@ -42,10 +43,15 @@ class Building(Elevator):
         self.elevator = Elevator(floor_bottom, floor_top)
     def run_elevator(self, accept_floor, accept_elevator):
         print(f"You chosen No.{accept_elevator} elevator.")
-        self.elevator.go_to_floor(accept_floor+1)
+        self.elevator.elevator_move(accept_floor+1)
         
 
 
 new_building = Building(1,8,5)
 
 new_building.run_elevator(2,3)
+
+new_building.run_elevator(9,3) #top floor out of range
+new_building.run_elevator(0,3) #bottom floor out of range
+new_building.run_elevator(-1,3) #bottom floor out of range negative number test
+new_building.run_elevator(2,-1) #elevator out of range negative number test
